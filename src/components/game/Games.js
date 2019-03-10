c0b78182280846d72a6a1483bc99afd2a4e62cea
Vagner Zampieri <vagner.zampieri@myfreecomm.com.br>
2019-03-10 07:02:11 -0300

    Add components Games

--- src/components/game/Games.js
+++ src/components/game/Games.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CSSModules from 'react-css-modules';
+import { Table } from 'semantic-ui-react';
 
 import styles from './Games.scss';
 
@@ -7,8 +8,34 @@ import styles from './Games.scss';
 class Games extends Component {
   render() {
     return (
-      <div styleName="games">
-        
+      <div className="ui container">
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Name</Table.HeaderCell>
+              <Table.HeaderCell>Round</Table.HeaderCell>
+              <Table.HeaderCell>Wildling Threat</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>Game #1</Table.Cell>
+              <Table.Cell>1</Table.Cell>
+              <Table.Cell>2</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Game #2</Table.Cell>
+              <Table.Cell>1</Table.Cell>
+              <Table.Cell>2</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Game #3</Table.Cell>
+              <Table.Cell>1</Table.Cell>
+              <Table.Cell>2</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
       </div>
     );
   }
